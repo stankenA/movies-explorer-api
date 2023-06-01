60ba1a3a9ed160f025faf863042f1767e903117d
stanken_a <artyemst3@mail.ru>
2023-06-01 12:11:27 +0300

    feat: add app.js with additional packages + cors and logger

---
+++ app.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const { errors } = require('celebrate');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+// const routes = require('./routes/routes');
+// const сentralizedErrors = require('./middlewares/errors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const cors = require('./middlewares/cors');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+app.use(express.json()); // для сборки JSON-формата
+app.use(express.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
+
+mongoose.connect('mongodb://localhost:27017/movies', {
+  family: 4,
+});
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+app.use(requestLogger);
+app.use(cors);
+
+app.use(limiter);
+app.use(helmet());
+// app.use('/', routes);
+
+app.use(errorLogger);
+
+app.use(errors());
+// app.use(сentralizedErrors);
+
+app.listen(PORT);
---
+++ utils/constants.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-useless-escape */
+const REGEXP_URL = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+
+const allowedCors = [
+  // 'http://stankena-mesto.nomoredomains.monster',
+  // 'https://stankena-mesto.nomoredomains.monster',
+  // 'http://api.stankena-mesto.nomoredomains.monster',
+  // 'https://api.stankena-mesto.nomoredomains.monster',
+  'localhost:3000',
+  'http://localhost:3000',
+];
+
+module.exports = {
+  REGEXP_URL,
+  allowedCors,
+};
