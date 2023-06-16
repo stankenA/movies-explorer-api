d3038d41c3efb81d6507029a3ac0375a77ee6330
stanken_a <artyemst3@mail.ru>
2023-06-16 13:35:54 +0300

    fix: add correct cors

--- utils/constants.js
+++ utils/constants.js
@@ -3,8 +3,8 @@ const REGEXP_URL = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9(
 const allowedCors = [
   'http://api.movies-exporer.nomoredomains.rocks',
   'https://api.movies-exporer.nomoredomains.rocks',
-  'localhost:3001',
-  'http://localhost:3001',
+  'localhost:3000',
+  'http://localhost:3000',
 ];
 
 module.exports = {
