092274c3f39a6f829f7f8018e9d71101d408f308
stanken_a <artyemst3@mail.ru>
2023-06-23 17:08:50 +0300

    fix: fix allowed cors

--- utils/constants.js
+++ utils/constants.js
@@ -1,11 +1,10 @@
 const REGEXP_URL = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 
 const allowedCors = [
-  'http://api.movies-exporer.nomoredomains.rocks',
-  'https://api.movies-exporer.nomoredomains.rocks',
+  'http://movies-exporer.nomoredomains.rocks',
+  'https://movies-exporer.nomoredomains.rocks',
   'localhost:3000',
   'http://localhost:3000',
-  'http://localhost:3000/',
 ];
 
 module.exports = {
